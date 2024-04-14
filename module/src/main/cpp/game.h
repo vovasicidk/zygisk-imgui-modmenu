17eeb01f696ed6d15feec907883d3b6adb4f86e1
vovasicidk <165401281+vovasicidk@users.noreply.github.com>
2024-04-14 20:38:51 +0300

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.axlebolt.standoff2"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
